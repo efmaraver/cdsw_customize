62871a909b55a65c11fdbded5ed9f47a9a028094
Fernandez Maraver <efmaraver@minsait.com>
2020-04-24 13:51:33 +0200

    create src folder

--- src/predict_boost.py
+++ src/predict_boost.py
@@ -1,12 +1,12 @@
 # Read the fitted model from the file model.pkl
 # and define a function that uses the model to
 # predict petal width from petal length
-
+import numpy as np
 import pickle
 
 model = pickle.load(open('model.pkl', 'rb'))
 
 def predict(args):
-  petal_length = float(args.get('petal_length'))
-  result = model.predict([[petal_length]])
-  return result[0][0]
+  X = np.array(args.get('datos'))
+  result = model.predict(X)
+  return result
