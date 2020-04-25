eac475af014ce48f2863fcab078de293145d40c0
Fernandez Maraver <efmaraver@minsait.com>
2020-04-25 19:08:44 +0200

    app.py

---
+++ app/appi.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import numpy as np
+from model import NLPModel
+from predict_boost import predict
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+class MakePrediction(Resource):
+    @staticmethod
+    def post():
+        args = request.get_json()
+        pred = predict(args)
+        result = jsonify ({"result": pred})
+        return result
+
+
+api.add_resource(PredictBoost, '/predict')
+
+if __name__ == '__main__':
+    app.run(debug=True)
